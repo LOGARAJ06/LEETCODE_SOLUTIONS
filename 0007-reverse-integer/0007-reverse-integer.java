5f10ee0f15336da0a837333a0b2aa4431affd717
LOGARAJ06 <logarajlogu06@gmail.com>
2024-09-24 22:27:37 +0530

    Time: 0 ms (100%), Space: 40.8 MB (46.04%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int reverse(int x) {
+       int rev=0;
+       while(x!=0){
+        int rem=x%10;
+        if(rev>Integer.MAX_VALUE/10 || rev<Integer.MIN_VALUE/10){
+            return 0;
+        }
+        rev = (rev*10)+rem;
+        x=x/10;
+       } 
+       return rev;
+    }
+}
