0bd288f142f073020d8a9e95352e420969f5b71a
LOGARAJ06 <logarajlogu06@gmail.com>
2024-12-12 21:36:03 +0530

    Time: 3 ms (90.17%), Space: 44.6 MB (41.83%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+        char[] ch1 = s.toCharArray();
+        char[] ch2 = t.toCharArray();
+        Arrays.sort(ch1);
+        Arrays.sort(ch2);
+        return Arrays.equals(ch1,ch2);
+    }
+}
