492ace842dba0f490174a9b97e72e4dc0a83fe82
LOGARAJ06 <logarajlogu06@gmail.com>
2024-12-13 12:32:35 +0530

    Time: 1 ms (67.8%), Space: 40.7 MB (53.44%) - LeetHub

--- 2571-find-the-pivot-integer/2571-find-the-pivot-integer.java
+++ 2571-find-the-pivot-integer/2571-find-the-pivot-integer.java
@@ -4,14 +4,14 @@ class Solution {
        int rightsum=0;
        for(int i=1;i<=n;i++){
         rightsum+=i;
-       } 
+       }
        for(int i=1;i<=n;i++){
         int val=i;
-        rightsum-=i;
+        rightsum-=val;
         if(leftsum==rightsum){
-            return i;
+            return val;
         }
-        leftsum+=i;
+        leftsum+=val;
        }
        return -1;
     }
