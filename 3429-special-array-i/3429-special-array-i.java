7610ba31e3f955f83487da18157c39173a8273c5
LOGARAJ06 <logarajlogu06@gmail.com>
2025-02-01 22:04:36 +0530

    Time: 0 ms (100%), Space: 43.5 MB (43.68%) - LeetHub

---
+++ 3429-special-array-i/3429-special-array-i.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isArraySpecial(int[] nums) {
+        if(nums.length==1){
+            return true;
+        }
+
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]%2==0 && nums[i+1]%2==0 || nums[i]%2!=0 && nums[i+1]%2!=0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
