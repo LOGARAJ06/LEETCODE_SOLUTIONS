81ce1a58a96e22ece25c0c7eaee5ef2b402b6c43
LOGARAJ06 <logarajlogu06@gmail.com>
2025-07-02 19:20:57 +0530

    Time: 0 ms (100%), Space: 41.8 MB (48.05%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,18 +1,14 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        
-        int length=0;
-
-        for(int i=s.length()-1; i>=0 ;i--)
-        {
-            if(s.charAt(i)!=' '){
-                length++;
-            }else{
-                if(length>0)
-                    return length;
+        s = s.stripTrailing();
+        int length = 0;
+        for(int i=s.length()-1;i>=0;i--){
+            if(s.charAt(i)==' '){
+                return length;
             }
+            length++;
         }
-    return length;
+        return length;
     }
 }
 
