ccfcb2aad63ccefcfa84046d58383e134384f2a6
LOGARAJ06 <logarajlogu06@gmail.com>
2025-07-13 21:06:42 +0530

    Time: 0 ms (100%), Space: 40.8 MB (63.64%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,14 +1,6 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        if(n==0){
-            return false;
-        }
-        if(n==1){
-            return true;
-        }
-        if(n%2!=0){
-                return false;
-            }
-    return isPowerOfTwo(n/2);
+    return (n>0 )&& (n&(n-1))==0;
+
     }
 }
